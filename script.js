28b57a08ce4ffc8a67f52279b1815578054d8119
Michael Kim <35280202+mkim120@users.noreply.github.com>
2023-09-28 16:02:31 -0400

    dark mode updates

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const body = document.querySelector('body');
+const nav = document.querySelector('nav');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
 
 const modeToggle = document.getElementById('mode-toggle');
 
+const border = document.getElementById('dark-mode-border');
+
+const card = document.querySelectorAll('.card-footer');
+
 function toggleMode() {
   body.classList.toggle('dark-mode');
+  nav.classList.toggle('navbar-dark');
   main.classList.toggle('dark-mode');
   footer.classList.toggle('dark-mode');
+
+  if (border.style.display === 'block') {
+    border.style.display = 'none';
+  } else {
+    border.style.display = 'block';
+  }
+
+  for (let e = 0; e < card.length; e++) {
+    card[e].classList.toggle('bg-dark');
+  }
 }
 
 modeToggle.addEventListener('click', toggleMode);
