18de39671848a9d27fc96e65d5c4b3bb921ef802
Michael Kim <35280202+mkim120@users.noreply.github.com>
2023-09-30 21:09:53 -0400

    added fade-and-slide-in effect and extra project

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ function toggleMode() {
 }
 
 modeToggle.addEventListener('click', toggleMode);
+
+// fade and slide in on scroll
+
+const items = document.querySelectorAll('.item');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
