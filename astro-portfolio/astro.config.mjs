bf3dba69af897e66652f3386777680118beaf63d
Michael Kim <35280202+mkim120@users.noreply.github.com>
2023-10-12 15:49:23 -0400

    astro deploy to github pages

--- astro-portfolio/astro.config.mjs
+++ astro-portfolio/astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://mkim120.github.io',
+});
