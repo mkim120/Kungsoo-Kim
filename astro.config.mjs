757281b0b67fa4c10888082fe2449ef4251b6df9
Michael Kim <35280202+mkim120@users.noreply.github.com>
2023-10-12 16:57:21 -0400

    update jekyll and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mkim120.github.io',
+    site: 'https://mkim120.github.io'
 });
